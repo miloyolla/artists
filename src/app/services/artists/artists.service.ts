f2beb22c4f06877c5c763d91d0f65d13c6390561
Mileny <mileny.loyolla@gmail.com>
2021-02-17 18:54:14 -0300

    (add) getArtists na service artists

---
+++ src/app/services/artists/artists.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArtistsService {
+  ids= "2CIMQHirSU0MQqyYHq0eOx%2C57dN52uHvrHOxijzpIgu3E%2C1vCWHaC5f2uS3yhpwWbIA6,0oSGxfWSnnOXhD2fKuz2Gy,3dBVyJ7JuOMt4GE9607Qin";
+
+  httpHeaders: any = {
+      headers: {
+        'Content-type': 'application/json',
+        'Accept': 'application/json',
+        // 'Authorization': 
+      }
+  }
+
+  constructor( public http: HttpClient ) { }
+
+  getArtists(): Observable<any>{
+    return this.http.get("https://api.spotify.com/v1/artists?ids="+ this.ids, this.httpHeaders);
+  }
+}
